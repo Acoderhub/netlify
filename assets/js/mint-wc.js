c13bbe6116ba47723aec54dbc6123f6ad8819e5b
Coder <77056476+Acoderhub@users.noreply.github.com>
2022-03-31 11:06:05 +0530

    Update mint-wc.js

--- assets/js/mint-wc.js
+++ assets/js/mint-wc.js
@@ -21,7 +21,7 @@ let tokensRemaining;
 // Address of the selected account
 let account;
 let contractNetwork = 4;
-let contractAddress = "0xB2d611A979CdD0C2da2e6f1c481173F741C7F5EE";
+let contractAddress = "0x23c96cc5E320a431f10dDD41546e141aF13157f7";
 
 let mintPrice = 30000000000000000;
 let mintPriceInEther = 0.03; // to be changed in section-2.js as well
