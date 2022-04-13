82472226589aca0d8c75edbb069c845b8e4fe972
Coder <77056476+Acoderhub@users.noreply.github.com>
2022-04-13 07:59:37 +0530

    Update section-2.js

--- assets/js/section-2.js
+++ assets/js/section-2.js
@@ -37,7 +37,7 @@ function setMintingWelcomeText() {
 }
 
 function setMintProgress(minted) {
-    const perMinted = minted / 10000 * 100; // Make percentage
+    const perMinted = minted / 4999 * 100; // Make percentage
     document.querySelector('#section-2-counter-progress').style.width = perMinted + '%';
 
     if (perMinted == 100) {
@@ -45,7 +45,7 @@ function setMintProgress(minted) {
         document.querySelector('p#section-2-counter-text').innerHTML = 'SOLD OUT';
     } else {
         // Update count text (insert comma using regex)
-        document.querySelector('p#section-2-counter-text span').innerHTML = (10000 - minted).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+        document.querySelector('p#section-2-counter-text span').innerHTML = (4999 - minted).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
     }
 }
 
